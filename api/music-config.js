5c02a48cef5ef93e3fc9a4eaf8f23910c3772506
Gog12 <bejop0433@gmail.com>
2026-05-26 08:59:52 +0700

    Create music-config.js

---
+++ api/music-config.js
@@ -0,0 +1,7 @@
+export const musicConfig = {
+  tech: ["tech1.mp3", "tech2.mp3"],
+  soft: ["soft1.mp3"],
+  upbeat: ["upbeat1.mp3"],
+  cinematic: ["industrial1.mp3"],
+  default: ["default.mp3"]
+};
